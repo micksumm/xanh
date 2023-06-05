c061d1adbec04c2713683c5a7018917c83e49ed3
Summer <summmick@gmail.com>
2023-06-05 12:55:23 -0700

    Initial commit

---
+++ src/test/java/com/xanh/XanhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xanh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XanhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
