95f50b38940439fb768834b63d9b89017dd5c7c3
Summer <summmick@gmail.com>
2023-06-15 15:51:32 -0700

    Add API endpoints and test for them

--- src/main/java/com/xanh/XanhApplication.java
+++ src/main/java/com/xanh/XanhApplication.java
@@ -1,22 +1,65 @@
 package com.xanh;
 
+import models.Plant;
+import models.requests.UpdatePlantRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 @RestController
 public class XanhApplication {
 
+    private ArrayList<Plant> plants = new ArrayList<>();
     public static void main(String[] args) {
         SpringApplication.run(XanhApplication.class, args);
     }
 
-    @GetMapping("/hello")
-    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+    @PostMapping("/plant/create")
+    public Plant createPlant(@RequestParam(value = "name") String name,
+                              @RequestParam(value = "sciName") String scientificName) {
+        Plant plant = new Plant(name, scientificName);
+        plants.add(plant);
+        return plant;
+    }
+
+    @GetMapping("/plants")
+    public ArrayList<Plant> getPlants() {
+        return plants;
+    }
+
+    @GetMapping("/plant")
+    public Plant getPlant(@RequestParam(value = "name") String name) {
+        for (Plant plant : plants) {
+            if (plant.name.equals(name)) {
+                return plant;
+            }
+        }
+        return null;
+    }
+
+    @PutMapping("/plant/edit")
+    public Plant editPlant(@RequestBody UpdatePlantRequest request) {
+        for (Plant plant : plants) {
+            if (plant.name.equals(request.plantName)) {
+                plant.name = request.updatedPlant.name;
+                plant.scientificName = request.updatedPlant.scientificName;
+                return plant;
+            }
+        }
+        return null;
+    }
+
+    @DeleteMapping("plant/delete")
+    public ArrayList<Plant> deletePlant(@RequestParam(value = "name") String name) {
+        for (Plant plant : plants) {
+            if (plant.name.equals(name)) {
+                plants.remove(plant);
+            }
+        }
+        return plants;
     }
 
 }
---
+++ src/main/java/models/Plant.java
@@ -0,0 +1,12 @@
+package models;
+
+public class Plant {
+    public String name;
+    public String scientificName;
+
+    public Plant(String name, String scientificName) {
+        this.name = name;
+        this.scientificName = scientificName;
+    }
+
+}
